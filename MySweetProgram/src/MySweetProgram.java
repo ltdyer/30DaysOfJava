111d603b3a235350214aee985f008ab12b4f0d02
Luke Dyer <ltdyer@email.wm.edu>
2020-08-18 11:44:57 -0400

    initial commit

---
+++ MySweetProgram/src/MySweetProgram.java
@@ -0,0 +1,15 @@
+
+public class MySweetProgram {
+	public static String howdy() {
+		/*
+		 * static means you don't need to invoke an object to use the method
+		 */
+		return("howdy");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println(howdy());
+	}
+
+}
