b7b5cbe7605b31af908b3d63ca5de89d4d713258
Luke Dyer <8luke.dyer8@gmail.com>
2020-08-18 14:32:51 -0400

    convert queue to stack

--- src/MySweetProgram.java
+++ src/MySweetProgram.java
@@ -2,30 +2,22 @@ import java.util.List;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class MySweetProgram {
-	public static String howdy() {
-		/*
-		 * static means you don't need to invoke an object to use the method
-		 */
-		return("howdy");
-	}
-	public static void main(String[] args) throws FileNotFoundException {
+	
 
-		Scanner in = new Scanner(new File("students.txt"));
+	public static void main(String[] args)  {
 		
-		List<String> students = new ArrayList<String>();
 		
-		while(in.hasNextLine()) {
-			students.add(in.nextLine());
-		}
+
+		LinkedList<String> names = new LinkedList<String>();
+		names.pop();
+		names.pop();
+		names.pop();
 		
-		for(int i = 0; i < students.size(); i++) {
-			System.out.println("name: " + students.get(i));
-		}
-		
-		in.close();
+		System.out.println(names.peek());
 	}
 	
 
